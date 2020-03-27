fd21c91823881a0348a2f272a4366709662c3f42
armylau <armylau@163.com>
2020-03-27 13:49:20 +0800

    add

--- pyplot.py
+++ pyplot.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
-x = np.linspace(0,1,300)
+
+x = np.linspace(0, 1, 300)
 print(x)
-for w in range(2,6,2):
-    plt.plot(x,np.sin(np.pi*x)*np.sin(2*w*np.pi*x) )
+for w in range(2, 6, 2):
+    plt.plot(x, np.sin(np.pi * x) * np.sin(2 * w * np.pi * x))
 print("plot done")
+print("add test")
